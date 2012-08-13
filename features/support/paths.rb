66b2f0b6bd8b8f55e3fb20511152a8cd3770303a
saasbook <saasbook@saasbook.(none)>
2012-08-13 07:46:20 -0700

    implement When I go to the edit page for

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,10 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+    when /^the edit page for "(.*)"$/i
+      m = Movie.find_by_title($1)
+      edit_movie_path(m)
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
